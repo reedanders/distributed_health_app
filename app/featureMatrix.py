80e1eb512a70baa30f6d80e1d68e79a3f53b81e9
ReedAnders <rean8874@colorado.edu>
2017-04-04 17:21:03 -0600

    working on post

---
+++ app/featureMatrix.py
@@ -0,0 +1,49 @@
+import numpy as np
+from scipy.spatial import distance, distance_matrix
+
+class FeatureMatrix(object):
+    """docstring for FeatureMatrix"""
+    def __init__(self):
+        super(FeatureMatrix, self).__init__()
+        self.matrix = self._load_matrix()
+        self.user_list = self._load_list()
+
+
+    def _load_matrix(self):
+        placeholder = np.array([[0.2, 0.1, 0.9, 0.5],\
+                                [0.9, 0.3, 0.8, 0.2],\
+                                [0.7, 0.6, 0.3, 0.2]])
+        return placeholder
+
+
+    # TODO: Better data structure
+    def _load_list(self):
+        placeholder = np.array(['A', 'B', 'C'])
+        return placeholder
+
+
+    # Update matrix and return neighbors as list
+    def match(self, user, vector):
+
+        user_index = np.where(self.user_list == user)
+
+        self.updateVector(user_index, vector)
+        neighbors = self._findNeighbors(user_index)
+
+        return neighbors
+
+
+    # Update user vector in matrix
+    # TODO Catch for new user, add new user
+    def updateVector(self, user_index, vector):
+        self.matrix[user_index] = vector
+
+
+    # Calcualte distance between users, return closest
+    # TODO Return list of nearest neighbors, not single neighbor
+    def _findNeighbors(self, user_index):
+        all_neighbors = distance.pdist(self.matrix, 'euclidean')
+        top_match = np.argmax(\
+            distance_matrix(self.matrix, self.matrix)[user_index])
+
+        return self.user_list[top_match]
--- app/views.py
+++ app/views.py
@@ -1,6 +1,26 @@
+from flask import request, jsonify
+
 from app import app
+from featureMatrix import FeatureMatrix
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/update', methods=['GET','POST'])
+def update():
+    import pdb; pdb.set_trace()
+    if request.method == 'POST':
+        user = request.args.get('user')
+        vector = request.args.get('vector')
+
+        # 'A'
+        # np.array([.1,.2,.3,.5])
+
+        fm = FeatureMatrix()
+
+        nearest_neighbor = fm.match(user, vector)
+
+        return jsonify(result='Success',
+                            data=nearest_neighbor)
