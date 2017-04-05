2b39937a7777e8fae0d6f13c9cf3bcedb84abb9f
hackwa <anurag6dubey@gmail.com>
2017-04-04 19:16:43 -0600

    add attrs

--- client.py
+++ client.py
@@ -13,9 +13,37 @@ class user(object):
         self.id = 'A'
         #self.id = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(10))
         self.vector = [.1,.2,.3,.5]
+        self.attrs = {
+        'Age' : None,
+        'Gender' : None,
+        'HeartRateAvg' : None,
+        'ExerciseHrs' : None,
+        'SleepHrs': None,
+        'SaltLv' : None,
+        'WaterIntake': None,
+        'DiseaseType' : None,
+        'DiseaseDuration': None,
+        'OtherData' : None
+        }
     
     def collect_data(self):
-        None
+        """
+        Set Random Values For Now
+        """
+        self.attrs['Age'] = 25
+        # 0 = Male 1 = Female
+        self.attrs['Gender'] = 1
+        self.attrs['HeartRateAvg'] = 75
+        self.attrs['ExerciseHrs'] = 2
+        self.attrs['SleepHrs'] = 7
+        self.attrs['SaltLv'] = 20
+        # Liters
+        self.attrs['WaterIntake'] = 3
+        # Need a disease type to number matching
+        self.attrs['DiseaseType'] = 420
+        # Months
+        self.attrs['DiseaseDuration'] = 24
+        self.attrs['OtherData'] = None
 
     def update_server(self):
         global endpoint
@@ -29,4 +57,7 @@ class user(object):
 
 hackwa = user("hackwa")
 print(hackwa.name,hackwa.id,hackwa.vector)
+hackwa.collect_data()
 hackwa.update_server()
+print(hackwa.attrs)
+print(list(hackwa.attrs.values()))
