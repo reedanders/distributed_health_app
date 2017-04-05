863e85dd1002722973c6d1347a2b0026f82f57bb
hackwa <anurag6dubey@gmail.com>
2017-04-04 22:08:38 -0600

    add authentication in server

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
-from flask import request, jsonify
+from flask import request, jsonify,abort
 import numpy as np
 
 from app import app
 from featureMatrix import FeatureMatrix
+
+registered_ids = {}
+logged_in=[]
 
 @app.route('/')
 @app.route('/index')
@@ -12,15 +15,46 @@ def index():
 @app.route('/update', methods=['POST'])
 def update():
     if request.method == 'POST':
-
         user = request.values['user']
-
         vector = request.values['vector']
+        if user not in logged_in:
+            abort(404)
         vector = np.fromstring(vector, dtype=float, sep=',')
-
         fm = FeatureMatrix()
         # nearest_neighbor = fm.match('A', np.array([.1,.2,.3,.5]))
         nearest_neighbor = fm.match(user, vector)
-
         return jsonify(result='Success',
                             neighbor=nearest_neighbor)
+
+@app.route('/login',methods=['POST'])
+def login():
+    if request.method == 'POST':
+        user = request.values['user']
+        pwd = request.values['password']
+        if user in logged_in:
+            return jsonify(result='Success')
+        if user in registered_ids.keys():
+            if registered_ids[user] == pwd:
+                print("logging in",user)
+                logged_in.append(user)
+                return jsonify(result='Success')
+        else:
+            abort(404)
+
+@app.route('/logout',methods=['POST'])
+def logout():
+    if request.method == 'POST':
+        user = request.values['user']
+        print("logging out",user)
+        logged_in.remove(user)
+        return jsonify(result='Success')
+
+@app.route('/register',methods=['POST'])
+def register():
+    if request.method == 'POST':
+        user = request.values['user']
+        pwd = request.values['password']
+        print("Registering",user)
+        if user not in registered_ids.keys():
+            registered_ids[user] = pwd
+        return jsonify(result='Success')
